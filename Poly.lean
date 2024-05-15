5f4a3db59e3f7901fb7926350ddc245b23ffb4d2
sinhp <sinahazratpour@gmail.com>
2024-05-15 17:53:52 +0200

    Update Poly.lean

--- Poly.lean
+++ Poly.lean
@@ -1,4 +1,4 @@
 -- This module serves as the root of the `Poly` library.
 -- Import modules here that should be built as part of the library.
-import Poly.LCCC
-import Poly.TypeUni
+import Poly.LCCC.LCCC
+import Poly.Type.Univariate
