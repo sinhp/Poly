4f87eec0197cf861fa974a617fc40e9ecec41a3a
Steve Awodey <sawodey@gmail.com>
2024-05-27 19:47:40 +0200

    added an outline

--- Poly/Presheaves/presheaves.lean
+++ Poly/Presheaves/presheaves.lean
@@ -1,5 +1,13 @@
 /-
-Copied from LCCC.lean
+Preamble copied and modified from LCCC.lean
+-/
+
+/-
+Here's a plan for this file to show that the category of presheaves on a (small) cat C is LCC:
+(1) define the category of presheaves on a (small) cat C and show it is a CCC,
+(2) the slice category over any presheaf is presheaves on its category of elements,
+(3) infer that every slice of presheaves is a CCC,
+(4) use the results from the LCCC development to infer that presheaves is LCC.
 -/
 
 import Mathlib.CategoryTheory.Category.Basic
@@ -8,6 +16,7 @@ import Mathlib.CategoryTheory.Closed.Cartesian
 import Mathlib.CategoryTheory.Adjunction.Mates
 import Mathlib.CategoryTheory.Limits.Constructions.Over.Basic
 import Mathlib.CategoryTheory.Adjunction.Over
+import Mathlib.CategoryTheory.Yoneda
 
 -- All the imports below are transitively imported by the above import.
 -- import Mathlib.CategoryTheory.Adjunction.Basic
@@ -19,6 +28,9 @@ import Mathlib.CategoryTheory.Adjunction.Over
 -- import Mathlib.CategoryTheory.Monoidal.OfHasFiniteProducts
 -- import Mathlib.CategoryTheory.Limits.Preserves.Shapes.BinaryProducts
 -- import Mathlib.CategoryTheory.Adjunction.Limits
+-- import Mathlib.CategoryTheory.Functor
+-- import Mathlib.CategoryTheory.Products.Basic
+-- import Mathlib.Data.ULift
 
 /-!
 # Presheaves
