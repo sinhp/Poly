e965d3c5815ac1fa7f97ffbd1411b9ffa69e4382
Steve Awodey <sawodey@gmail.com>
2024-06-10 14:29:48 +0200

    added some references

--- Poly/Presheaves/presheaves.lean
+++ Poly/Presheaves/presheaves.lean
@@ -121,3 +121,28 @@ end CategoryOfElements
 # 5. Presheaves is an LCCC
 Use results in the file on locally cartesian closed categories.
 -/
+
+/-!
+# Some references
+
+1. Wellfounded trees and dependent polynomial functors.Gambino, Nicola; Hyland, Martin. Types for proofs and programs. International workshop, TYPES 2003, Torino, Italy, 2003. Revised selected papers.. Springer Berlin, 2004. p. 210-225.
+
+2. Tamara Von Glehn. Polynomials and models of type theory. PhD thesis, University of Cambridge, 2015.
+
+3. Joachim Kock. Data types with symmetries and polynomial functors over groupoids. Electronic Notes in Theoretical Computer Science, 286:351–365, 2012. Proceedings of the 28th Conference on the Mathematical Foundations of Programming Semantics (MFPS XXVIII).
+
+4. Jean-Yves Girard. Normal functors, power series and λ-calculus. Ann. Pure Appl. Logic, 37(2):129–177, 1988. doi:10.1016/0168-0072(88)90025-5.
+
+5. David Gepner, Rune Haugseng, and Joachim Kock. 8-Operads as analytic monads. Interna- tional Mathematics Research Notices, 2021.
+
+6. Nicola Gambino and Joachim Kock. Polynomial functors and polynomial monads. Mathematical Proceedings of the Cambridge Philosophical Society, 154(1):153–192, 2013.
+
+7. Marcelo Fiore, Nicola Gambino, Martin Hyland, and Glynn Winskel. The cartesian closed bicategory of generalised species of structures. J. Lond. Math. Soc. (2), 77(1):203–220, 2008.
+
+8. M. Fiore, N. Gambino, M. Hyland, and G. Winskel. Relative pseudomonads, Kleisli bicategories, and substitution monoidal structures. Selecta Mathematica, 24(3):2791–2830, 2018.
+
+9. Steve Awodey and Clive Newstead. Polynomial pseudomonads and dependent type theory, 2018.
+
+10. Steve Awodey, Natural models of homotopy type theory, Mathematical Structures in Computer Science, 28 2 (2016) 241-286, arXiv:1406.3219.
+
+-/
