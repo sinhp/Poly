4aa0ce138198570268704d194c498e9d9d999ed8
sinhp <sinahazratpour@gmail.com>
2024-06-18 11:04:53 +0200

    Poly.Type.Univariate not needed for now gives strange errors

--- Poly.lean
+++ Poly.lean
@@ -1,7 +1,7 @@
 -- This module serves as the root of the `Poly` library.
 -- Import modules here that should be built as part of the library.
 import Poly.LCCC.Basic
-import Poly.Type.Univariate
+-- import Poly.Type.Univariate
 import Poly.LCCC.Basic
 import Poly.Exponentiable
 import Poly.Polynomial
