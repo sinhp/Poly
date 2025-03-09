8185efb406d1df747d169b131a5e95038d850900
sinhp <sinahazratpour@gmail.com>
2025-03-09 21:58:29 +0100

    using Poly.ForMathlib.

--- Poly.lean
+++ Poly.lean
@@ -1,5 +1,7 @@
 import Poly.ForMathlib.CategoryTheory.LocallyCartesianClosed.Basic
 import Poly.ForMathlib.CategoryTheory.LocallyCartesianClosed.BeckChevalley
+import Poly.ForMathlib.CategoryTheory.LocallyCartesianClosed.Distributivity
+import Poly.ForMathlib.CategoryTheory.LocallyCartesianClosed.Presheaf
 -- import Poly.LCCC.BeckChevalley
 -- import Poly.LCCC.Presheaf
 -- import Poly.Type.Univariate
