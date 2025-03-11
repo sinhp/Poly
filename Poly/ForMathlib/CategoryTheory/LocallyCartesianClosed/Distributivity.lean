5f5deb1d780787e4469f7216ee475f9fb5d59703
sinhp <sinahazratpour@gmail.com>
2025-03-11 15:04:54 +0100

    Cartesian NatTrans

--- Poly/ForMathlib/CategoryTheory/LocallyCartesianClosed/Distributivity.lean
+++ Poly/ForMathlib/CategoryTheory/LocallyCartesianClosed/Distributivity.lean
@@ -3,8 +3,10 @@ Copyright (c) 2025 Sina Hazratpour. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sina Hazratpour, Emily Riehl
 -/
-import Poly.ForMathlib.CategoryTheory.LocallyCartesianClosed.Basic
 import Mathlib.CategoryTheory.Limits.Shapes.Pullback.CommSq
+import Poly.ForMathlib.CategoryTheory.LocallyCartesianClosed.Basic
+import Poly.ForMathlib.CategoryTheory.NatTrans
+
 
 /-!
 # Pentagon distributivity
