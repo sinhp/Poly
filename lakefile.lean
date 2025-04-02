eb5e25417b2836a04744e31af657bd47bcf8db23
sinhp <sinahazratpour@gmail.com>
2025-04-02 09:02:19 +0200

    Setup blueprint

--- lakefile.lean
+++ lakefile.lean
@@ -16,3 +16,9 @@ require seq from git "https://github.com/Vtec234/lean4-seq"
 @[default_target]
 lean_lib «Poly» where
   -- add any library configuration options here
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
